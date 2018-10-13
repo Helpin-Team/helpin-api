a99f94c9e5213bc48d85113b7f54348cb76c9e65
Marcos Dissotti <marcosdissotti@gmail.com>
2018-10-13 12:34:03 -0300

    ERM Adicionado modelagem do banco de dados

--- v1/app/Controllers/Http/KnowledgeController.js
+++ v1/app/Controllers/Http/KnowledgeController.js
@@ -1,55 +1,38 @@
 'use strict'
 
-/**
- * Resourceful controller for interacting with knowledges
- */
+
+const Knowledge = use('App/Models/Knowledge')
 class KnowledgeController {
-  /**
-   * Show a list of all knowledges.
-   * GET knowledges
-   */
-  async index ({ request, response, view }) {
+
+  async index () {
+    return await Knowledge
+      .query()
+      .setVisible([
+        'id',
+        'name',
+        'about'
+      ])
+      .fetch()
   }
 
-  /**
-   * Render a form to be used for creating a new knowledge.
-   * GET knowledges/create
-   */
-  async create ({ request, response, view }) {
+  async store ({ request }) {
+    const data = request
+      .only([
+        'id',
+        'name',
+        'about'
+      ])
+    return await Knowledge.create(data)
   }
 
-  /**
-   * Create/save a new knowledge.
-   * POST knowledges
-   */
-  async store ({ request, response }) {
+  async show ({ params }) {
+    const {id} = params
+    return await Knowledge
+      .query()
+      .where('id', id)
+      .fetch()
   }
 
-  /**
-   * Display a single knowledge.
-   * GET knowledges/:id
-   */
-  async show ({ params, request, response, view }) {
-  }
-
-  /**
-   * Render a form to update an existing knowledge.
-   * GET knowledges/:id/edit
-   */
-  async edit ({ params, request, response, view }) {
-  }
-
-  /**
-   * Update knowledge details.
-   * PUT or PATCH knowledges/:id
-   */
-  async update ({ params, request, response }) {
-  }
-
-  /**
-   * Delete a knowledge with id.
-   * DELETE knowledges/:id
-   */
   async destroy ({ params, request, response }) {
   }
 }
--- v1/app/Models/Knowledge.js
+++ v1/app/Models/Knowledge.js
@@ -3,6 +3,9 @@
 const Model = use('Model')
 
 class Knowledge extends Model {
+    student () {
+        return this.belongsTo('App/Models/Student')
+      }
 }
 
 module.exports = Knowledge
